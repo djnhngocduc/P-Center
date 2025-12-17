078038792cdf4170c0f72ce58d232b3973850473
Đinh Ngọc Đức <22028166@vnu.edu.vn>
2025-12-17 22:25:17 +0700

    update

--- experiments.py
+++ experiments.py
@@ -851,8 +851,8 @@ def parse_args():
     ap.add_argument(
         "--encodings",
         nargs="+",
-        default=["pysat_sc", "pypb_adder", "nsc", "pb_bdd"],
-        help="Encodings to test: pysat_sc pypb_adder nsc pb_bdd",
+        default=["pysat_sc", "pypb_bdd", "nsc", "pb_bdd"],
+        help="Encodings to test: pysat_sc pypb_bdd nsc pb_bdd",
     )
     ap.add_argument(
         "--solvers",
